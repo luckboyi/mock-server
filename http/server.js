e0196be197ef96a8c3bb8e64a281b43900c69bd6
luckboyi <546217953@qq.com>
2019-05-24 18:46:01 +0800

    提交封装的ajax方法

---
+++ http/server.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import qs from 'qs';
+let http = {
+  post: '',
+  get: '',
+};
+http.post = (api, data) => {
+  let params = qs.stringify(data);
+  return new Promise((resolve, reject) => {
+    axios.post(api, params).then(res => {
+      resolve(res);
+    });
+  });
+};
+http.get = (api, data) => {
+  let params = qs.stringify(data);
+  return new Promise((resolve, reject) => {
+    axios.get(api, params).then(res => {
+      resolve(res);
+    });
+  });
+};
+export default http;
